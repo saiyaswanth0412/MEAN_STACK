3c1670c1a1c170aa16bc7bef18cbaadb0a445897
saiyaswanth0412 <60351767+saiyaswanth0412@users.noreply.github.com>
2023-02-02 18:26:11 +0530

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-const express=require('express');
-const app=express();
-app.get('/',(req,res)=>{
-    res.send("Hello world");
-})
-app.listen(3000,()=>{
-    console.log("the server is listening on port 3000");
-})
